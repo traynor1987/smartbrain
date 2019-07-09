ec1a427113473187b21ba13f9fb5ff97a57c2359
James Traynor <>
2019-07-09 19:21:39 +0100

    added all components

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+            {'James, your current Rank is...'}
+            </div>
+            <div className='white f1'>
+            {'#5'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
